eec34041b6ab333ee2b369cef1c575b542365fc1
Isaah Liu <isaah.liu@purple.telstra.com>
2022-08-08 15:37:48 +1000

    wip

--- RxPrimer/Part3.cs
+++ RxPrimer/Part3.cs
@@ -7,7 +7,7 @@ public class Part3
     {
         bool IsEven(int i) => i % 2 == 0;
         var count = 0;
-        
+
         var sut =
             Observable
                 .Create<int>(o =>
@@ -25,22 +25,51 @@ public class Part3
                 .Where(IsEven)
                 .DistinctUntilChanged();
 
-        sut.Subscribe(i =>
-        {
-            Assert.Equal(__, i);
-            count += 1;
-        });
-        
+        sut
+            .Do(i => count += 1)
+            .Subscribe(i => Assert.Equal(__, i));
+
         Assert.Equal(__, count);
+        Assert.Equal(10, count);
+    }
+
+    [Fact]
+    public void count_with_aggregate()
+    {
+        var source = Observable.Range(0, 10);
+        var sut = source
+            .Aggregate(0, (acc, next) => acc + 1);
+        var count = 0;
+        sut
+            .Do(_ => count += 1)
+            .Subscribe(i => Assert.Equal(__, i),
+            () => Assert.Equal(__, count));
+    }
+
+    [Fact]
+    public void max_with_scan()
+    {
+        var source = Observable.Range(0, 5);
+        var sut = source
+            .Scan(0, (acc, next) => next > acc ? next : acc);
+        var count = 0;
+        var result = string.Empty;
+        sut
+            .Do(_ => count += 1)
+            .Select(i => result += i)
+            .Subscribe(i => { },
+                () => Assert.Equal(__, count));
+        // Scan isn't blocking, why are you seeing what you see?
+        Assert.Equal(___, result);
     }
     
     
-    
+
     #region Ignore
 
     private static object ____ = "Please fill in the blank.";
     private static int __ = 100;
     private static string ___ = "Please fill in the blank.";
-    
+
     #endregion
 }
